9c10235a592a14a667f749741b1e89b9ea2f9eda
SYNNN47 <francesco.refo@binus.ac.id>
2024-12-10 12:37:37 +0700

    hold jump fix

---
+++ Assets/Scripts/CatBehaviour.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatBehaviour : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+
+    [Header("Jump Settings")]
+    public float minJumpPower = 5f;     // Minimum jump power
+    public float maxJumpPower = 15f;   // Maximum jump power
+    public float chargeSpeed = 10f;    // How quickly jump power charges
+
+    private bool isGrounded = false;
+    private bool isChargingJump = false;
+    private float currentJumpPower;
+    private bool isFacingRight = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        currentJumpPower = minJumpPower;
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    void HandleMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        // Flip the sprite
+        if ((horizontalInput > 0 && !isFacingRight) || (horizontalInput < 0 && isFacingRight))
+        {
+            FlipSprite();
+        }
+
+        // Update animator
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    void HandleJump()
+    {
+        // Ground detection
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        animator.SetBool("isJumping", !isGrounded);
+
+        if (isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                // Start charging jump
+                isChargingJump = true;
+                currentJumpPower = minJumpPower;
+            }
+
+            if (Input.GetButton("Jump") && isChargingJump)
+            {
+                // Charge jump power up to the max value
+                currentJumpPower += chargeSpeed * Time.deltaTime;
+                currentJumpPower = Mathf.Clamp(currentJumpPower, minJumpPower, maxJumpPower);
+            }
+
+            if (Input.GetButtonUp("Jump") && isChargingJump)
+            {
+                // Release jump
+                rb.velocity = new Vector2(rb.velocity.x, currentJumpPower);
+                isChargingJump = false;
+                animator.SetBool("isJumping", true);
+            }
+        }
+    }
+
+    void FlipSprite()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Visualize ground check
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, 0.1f);
+    }
+
+
+
+
+
+}
