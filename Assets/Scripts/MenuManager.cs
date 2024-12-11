8b623214069eb4ba516bde65c75a7c2b2ce5a282
SYNNN47 <francesco.refo@binus.ac.id>
2024-12-11 20:48:13 +0700

    adding button

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,21 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ToggleStart()
     {
         SceneManager.LoadScene("Scene1");
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
 
